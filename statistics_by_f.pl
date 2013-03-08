3866fb4d86eaf307faf697456196647db4ecfcee
ram <rhombusj@gmail.com>
2013-03-07 21:30:12 -0500

    tested and cleaned up perl statistics scripts

--- statistics_by_f.pl
+++ statistics_by_f.pl
@@ -1,134 +1,107 @@
 #!/usr/bin/perl
+use Carp::Assert;
 #Usage
-#./statistics.pl [freq] [directory] [by_bm] [tec]
-#./statistics.pl [freq] [directory] [by_bm]
-#Processes data by benchmark
+#./statistics.pl [regex] [directory] 
+#Processes data files matching regex in the directory
+#@author Sriram Jayakumar
+#@date 3/7/2013
 
-$num_args = @ARGV;
-if (($num_args != 3) && ($num_args != 2)) { die "2 or 3 arguments expected; $num_args: @ARGV provided\n"; }
-$freq = $ARGV[0]; #frequency or benchmark
-$dirname = $ARGV[1];
-if ($num_args == 3) { $tec = $ARGV[2]; } else { $tec = ""; } #"tec_" or ""
+#Argument Parsing
+tester();
+my $num_args = @ARGV;
+if ($num_args != 2) { die "2 arguments expected; $num_args: @ARGV provided\n"; }
+my $regex = $ARGV[0]; #frequency or benchmark
+my $dirname = $ARGV[1];
 
+#File Preparation
 print "@ARGV\n";
-$output_file = join "","summary_",$tec,$freq,g,".csv";
+my $output_file = join "","summary_",$regex,".csv";
 system("rm -f $dirname\/$output_file");
 open(OUTPUT_FILE, ">>", "$dirname\/$output_file") or die "can't open $dirname\/$output_file for writing: $!\n";
 print OUTPUT_FILE "benchmark,avg_T[C],min,max,meandev,initial,avg_shunt_v[V],"
 	. "avg_fan_rpm, core[i] T, core[i] Tmin, core[i] Tmax, core[i] Tmeandev,"
-	. "core[i] Tinitial\n";
+	. "core[i] Tinitial, tec[i] voltage [V], tec[i] voltage meandev,"
+	. "tec[i] current [A], tec[i] current meandev, tec[i] power [W]\n";
 
 opendir(DIR, $dirname) or die "can't opendir $dirname: $!";
-$regex = $freq;
 
-@files = (); 
+my @files = (); 
 while (defined($file = readdir(DIR)))
 {
 	push(@files, $file);
 }
-@files_sorted = (sort @files);
+my @files_sorted = (sort @files);
 closedir(DIR);
 
-$num_cores = 4;
+#Parameters
+my $num_cores = 4;
+my $num_tec = 2;
 
-foreach $file (@files_sorted)
+#Data
+foreach my $file (@files_sorted)
 {
  	if ($file =~ m/\Q$regex/ && $file !~ m/summary/)
 	{
 		print "$file\n";
 		open(FILE, "<", "$dirname\/$file") or die "can't open $file for reading: $!\n";
 
-		$t_min = inf;
-		$t_max = -inf;
-		$t_sum = 0;
-		@temperatures = ();		
-		@core_temps = (); 
-		@core_tmin = ();
-		@core_tmax = ();
-		@core_tsum = ();
-		for ($core=0; $core < $num_cores; $core++) 
+		my @temperatures = ();		
+		my @core_temps = (); 
+		my @tec_v = ();
+		my @tec_i = ();
+		my @voltages = ();
+		my @rpms = ();
+
+		while (defined(my $line = <FILE>))
 		{
-			@core_tmin[$core] = inf;
-			@core_tmax[$core] = -inf;
-			@core_tsum[$core] = 0;
-		}
-
-		$v_sum = 0;
-		@voltages = ();
-
-		$rpm_sum = 0;
-		$num_rpm = 0;
-
-		while (defined($line = <FILE>))
-		{
-			@columns = split(/\s+/, $line);
+			my @columns = split(/\s+/, $line);
 
 			#Average T over all cores
-			$t =($columns[0]+$columns[1]+$columns[2]+$columns[3])/4;
+			my $t =($columns[0]+$columns[1]+$columns[2]+$columns[3])/4;
+			push @temperatures,$t;
 			
 			#Per core statistics
-			for ($core=0; $core < $num_cores; $core++) 
+			for (my $core=0; $core < $num_cores; $core++) 
 			{
-				$t_core = $columns[$core];
-				push @{$core_temps[$core]}, $t_core;
-				if($t_core < $core_tmin[$core])
-				{
-					$core_tmin[$core] = $t_core;
-				}	
-				if($t_core > $core_tmax[$core])
-				{
-					$core_tmax[$core] = $t_core;
-				}
-				$core_tsum[$core] = $core_tsum[$core] + $t_core;	
+				push @{$core_temps[$core]}, $columns[$core];
 			}
 			
+			#Regulator voltage
+			push(@voltages, $columns[4]);
 
-			push(@temperatures, $t);
-			if ($t < $t_min)
-			{
-				$t_min = $t;
-			}
-			if ($t > $t_max)
-			{
-				$t_max = $t;
-			}
-			$t_sum = $t_sum + $t;
-			
-			$v = $columns[4];
-			$v_sum = $v_sum + $v;
-			push(@voltages, $v);
+			#Fan speed
+			push(@rpms, $columns[6]);
 
-			$rpm_sum = $rpm_sum + $columns[6];
-			$num_rpm = $num_rpm + 1;
+			my $tec_start_index = 7;
+			for(my $k = 0; $k < $num_tec; $k++)
+			{
+				push @{$tec_v[$k]},$columns[$tec_start_index+$k*2];
+				push @{$tec_i[$k]},$columns[$tec_start_index+$k*2+1];
+			}
 		}
 		
-		$t_avg = $t_sum / (scalar @temperatures);
-		
-		$t_mean_dev_sum = 0;
-		foreach $i (@temperatures)
-		{
-			$t_mean_dev_sum = $t_mean_dev_sum + abs($i - $t_avg);
-		}
-		$t_mean_dev = $t_mean_dev_sum / (scalar @temperatures);
-
-		$v_avg = $v_sum / (scalar @voltages);
-		$rpm_avg = $rpm_sum / $num_rpm;
-
-		@file_parts = split(/_/, $file);
-		print OUTPUT_FILE "$file_parts[0] $file_parts[2],$t_avg,$t_min,$t_max,$t_mean_dev,$temperatures[0],$v_avg,$rpm_avg";	
+		print OUTPUT_FILE join ",",$file,compute_avg(@temperatures),
+			compute_min(@temperatures),compute_max(@temperatures),
+			compute_meandev(@temperatures),$temperatures[0],
+			compute_avg(@voltages),compute_avg(@rpms),"";
 
 		for ($core=0; $core < $num_cores; $core++) 
 		{
-			$t_core_avg = $core_tsum[$core] / (scalar @{$core_temps[$core]});
-			$core_meandev_sum = 0;
-			foreach $ct (@{$core_temps[$core]})
-			{
-				$core_meandev_sum = $core_meandev_sum 
-					+ abs($ct - $t_core_avg);
-			}
-			$core_meandev = $core_meandev_sum / (scalar @{$core_temps[$core]});
-			print OUTPUT_FILE ",$t_core_avg,$core_tmin[$core],$core_tmax[$core]," .
-				"$core_meandev, $core_temps[$core][0]";
+			print OUTPUT_FILE join ",",compute_avg(@{$core_temps[$core]}),
+				compute_min(@{$core_temps[$core]}),compute_max(@{$core_temps[$core]}),
+				compute_meandev(@{$core_temps[$core]}),$core_temps[$core][0],"";
+		}
+
+		#TEC voltage,current,power
+		for (my $k=0; $k<$num_tec; $k++)
+		{
+			my $avg_v, $avg_i;
+			$avg_v = compute_avg(@{$tec_v[$k]});
+			$avg_i = compute_avg(@{$tec_i[$k]});
+
+			print OUTPUT_FILE join ",", $avg_v,
+				compute_meandev(@{$tec_v[$k]}), $avg_i,
+				compute_meandev(@{$tec_i[$k]}), $avg_v*$avg_i,"";
 		}
 		print OUTPUT_FILE "\n";
 		close(FILE);
@@ -137,3 +110,77 @@ foreach $file (@files_sorted)
 }
 close(OUTPUT_FILE);
 
+sub compute_meandev
+{
+	my $meandev_sum, $meandev, $avg;
+	$meandev_sum = 0;
+	$meandev = 0;
+	$avg = compute_avg(@_);
+
+	foreach my $v (@_)
+	{
+		$meandev_sum += abs($v - $avg);
+	}
+	$meandev = $meandev_sum / (scalar @_);
+	return $meandev;
+}
+
+sub compute_avg
+{
+	my $sum,$avg;
+	$sum = 0;
+	$avg = 0;
+
+	foreach my $v (@_)
+	{
+		$sum += $v;
+	}
+	$avg = $sum / (scalar @_);
+	return $avg;
+}
+
+sub compute_max
+{
+	my $max = -inf;
+	foreach my $v (@_)
+	{
+		if($v > $max)
+		{
+			$max = $v;
+		}
+	}
+	return $max;
+}
+
+sub compute_min
+{
+	my $min = inf;
+	foreach my $v (@_)
+	{
+		if($v < $min)
+		{
+			$min = $v;
+		}
+	}
+	return $min;
+}
+
+#Test Cases
+#Additionally, for a real input file I checked the output
+#of this script against results from Excel. The results match.
+sub tester
+{
+	#simple
+	my @numbers = (-1..12);
+	assert(compute_avg(@numbers)==(77/14));
+	assert(compute_min(@numbers)==-1);
+	assert(compute_max(@numbers)==12);
+	assert(compute_meandev(@numbers)==3.5); #via MS Excel
+
+	#floating point, negative numbers
+	@numbers = (1.5,7.24,9.33,2.73,-54,1000);
+	assert(abs(compute_avg(@numbers)-161.133)<1e-3);
+	assert(compute_min(@numbers)==-54);
+	assert(compute_max(@numbers)==1000);
+	assert(abs(compute_meandev(@numbers)-279.622)<1e-3); #via MS Excel
+}
